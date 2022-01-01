be48ccc5f7462695a763b15f1881552bd1b8f3ea
jessonjava <jessbrisson@gmail.com>
2022-01-01 16:46:11 -0600

    Extract methods from class

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import './style.css';
 
 import { fromEvent } from 'rxjs';
-import { SimpleEventManager } from './simpleEventManager';
+import { emit, register, unregister, when } from './simpleEventManager';
 import { take } from 'rxjs/operators';
 
 const eventEmitter$ = fromEvent(document.getElementById('emit'), 'click');
@@ -9,16 +9,16 @@ const unsub$ = fromEvent(document.getElementById('unsub'), 'click');
 const unreg$ = fromEvent(document.getElementById('unreg'), 'click');
 
 // register an event with a name and initial state
-SimpleEventManager.register<number>('button-click', 0);
+register('button-click', 0);
 
 eventEmitter$.subscribe(() => {
-  SimpleEventManager.emit('button-click', (currentVal) => {
+  emit('button-click', (currentVal) => {
     return currentVal + 10;
   });
 });
 
 // listen for events with a specific name
-const subscription = SimpleEventManager.when('button-click', (val) => {
+const subscription = when('button-click', (val) => {
   console.log(val);
 });
 
@@ -28,17 +28,17 @@ unsub$.pipe(take(1)).subscribe(() => {
 });
 
 // add another listener to an event
-SimpleEventManager.when('button-click', () => {
+when('button-click', () => {
   console.log('another sub to button-click');
 });
 
 // unregister a whole event
 unreg$.pipe(take(1)).subscribe(() => {
-  SimpleEventManager.unregister('button-click');
+  unregister('button-click');
 });
 
 // try to use a non-existent event: logs a warning then quits
-SimpleEventManager.when('error', () => {});
+when('error', () => {});
 
 // try to register an already registered event: logs a warning then quits
-SimpleEventManager.register('button-click', 1000);
+register('button-click', 1000);
--- simpleEventManager.ts
+++ simpleEventManager.ts
@@ -1,52 +1,70 @@
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
-class SimpleEventManager {
-  private static registeredEvents: {
-    eventName: string;
-    subject: Subject<any>;
-  }[] = [];
+interface RegisteredEvent {
+  eventName: string;
+  subject: BehaviorSubject<any>;
+}
 
-  private static getEventFromEventName(eventName) {
-    return SimpleEventManager.registeredEvents.find((event) => {
-      return event.eventName === eventName;
-    });
+export default class SimpleEventManager {
+  private static _registeredEvents: RegisteredEvent[] = [];
+
+  static getEventFromEventName(eventName: string): RegisteredEvent | null {
+    return (
+      SimpleEventManager._registeredEvents.find((event) => {
+        return event.eventName === eventName;
+      }) ?? null
+    );
   }
 
-  static register<T>(eventName: string, initValue: T) {
-    if (SimpleEventManager.getEventFromEventName(eventName)) {
-      console.warn(`${eventName} already registered`);
-      return;
-    }
-    SimpleEventManager.registeredEvents.push({
-      eventName,
-      subject: new BehaviorSubject<T>(initValue),
-    });
+  static get registeredEvents(): RegisteredEvent[] {
+    return this._registeredEvents;
   }
-  static unregister(eventName: string) {
-    const event = SimpleEventManager.getEventFromEventName(eventName);
-    event.subject.complete();
-    SimpleEventManager.registeredEvents =
-      SimpleEventManager.registeredEvents.filter(
-        (event) => event.eventName !== eventName
-      );
-  }
-  static when(eventName: string, callback) {
-    const event = SimpleEventManager.getEventFromEventName(eventName);
-    if (!event) {
-      console.warn(`No event called ${eventName} registered`);
-      return;
-    }
-    return event.subject.subscribe(callback);
-  }
-  static emit(eventName: string, updatedValueCallback) {
-    const event = SimpleEventManager.getEventFromEventName(eventName);
-    if (!event) {
-      console.warn(`No event called ${eventName} registered`);
-      return;
-    }
-    const nextValue = updatedValueCallback(event.subject.getValue());
-    event.subject.next(nextValue);
+
+  static set registeredEvents(events: RegisteredEvent[]) {
+    this._registeredEvents = events;
   }
 }
 
-export { SimpleEventManager };
+export const emit = (
+  eventName: string,
+  updatedValueCallback: (value: any) => any
+) => {
+  const event = SimpleEventManager.getEventFromEventName(eventName);
+  if (!event) {
+    console.warn(`No event called ${eventName} registered`);
+    return;
+  }
+  const nextValue = updatedValueCallback(event.subject.getValue());
+  event.subject.next(nextValue);
+};
+
+export const register = (eventName: string, initValue: any) => {
+  if (SimpleEventManager.getEventFromEventName(eventName)) {
+    console.warn(`${eventName} already registered`);
+    return;
+  }
+  SimpleEventManager.registeredEvents.push({
+    eventName,
+    subject: new BehaviorSubject(initValue),
+  });
+};
+
+export const unregister = (eventName: string) => {
+  const event: RegisteredEvent | null =
+    SimpleEventManager.getEventFromEventName(eventName);
+  if (!event) return;
+  event.subject.complete();
+  SimpleEventManager.registeredEvents =
+    SimpleEventManager.registeredEvents.filter(
+      (event) => event.eventName !== eventName
+    );
+};
+
+export const when = (eventName: string, callback: (next: any) => any) => {
+  const event = SimpleEventManager.getEventFromEventName(eventName);
+  if (!event) {
+    console.warn(`No event called ${eventName} registered`);
+    return;
+  }
+  return event.subject.subscribe(callback);
+};
